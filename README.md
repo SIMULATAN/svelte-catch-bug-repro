63016bf689bfc8be7b32f4575855dee5154826ea
SIMULATAN <simulatan2319@gmail.com>
2024-02-16 21:10:54 +0100

    Initial reproducer

---
+++ README.md
@@ -0,0 +1,2 @@
+# Svelte Catch Bug Reproducer
+This repo contains a reproducer for a svelte bug that crashes the vite dev server entirely.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let data = new Promise((resolve, reject) => {
+        reject(new Error("Some error"));
+    });
+
+    let session = null;
+    setTimeout(() => {
+        session = "mysessionid";
+    }, 1000);
+</script>
+{#if session != null}
+    {#await data}
+        Loading...
+    {:then result}
+        Got a result: {result}
+    {:catch error}
+        Caught an error: {error}
+    {/await}
+{/if}
